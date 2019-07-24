24a2317de4815d3dced1bc2f5c25b80bf7039ced
Phani_Kiran <pret321@gmail.com>
2019-07-24 15:35:29 -0500

    Getting the value

--- savePhoneNumber/ViewController.swift
+++ savePhoneNumber/ViewController.swift
@@ -14,32 +14,39 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var enterNumber: UITextField!
     @IBOutlet weak var buttonTapped: UIButton!
     
-    
+    var abc: String = String()
+    var abd: String = String()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         self.enterNumber.delegate = self
         self.enterNumber.delegate = self
         print(UserDefaults.standard.dictionaryRepresentation())
-        let savedName = UserDefaults.standard.object(forKey: "PlayOneName")
-        var saveNumber = UserDefaults.standard.object(forKey: "numberSave")
-        
-        print((savedName)!)
+        abc = UserDefaults.standard.object(forKey: "PlayOneName") as! String
+        abd = UserDefaults.standard.object(forKey: "numberSave") as! String
+       
 
-        
     }
 
+    
     @IBAction func buttonOn(_ sender: Any) {
         //Below line will help you store the data inside  those key name
-        var names = enterName.text
-        var num = enterNumber.text
-        UserDefaults.standard.set(enterName.text, forKey: "PlayOneName")
+        var name = enterName.text
+        var abs = UserDefaults.standard.set(enterName.text, forKey: "PlayOneName")
         UserDefaults.standard.set(enterNumber.text, forKey: "numberSave")
-
-        
+        print(abc)
+        print(abd)
+        print(name!)
+        if abc == name{
+            print("yes")
+        }
+   
         
     }
 
+
+    
+    
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
